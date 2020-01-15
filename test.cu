3a4414a66b1e2a2bd55ea45aaa6e282f1f3598ea
Miguel Angel Ortiz Marin <miaortizma@unal.edu.co>
2020-01-15 10:39:18 -0500

    test

--- test.cu
+++ test.cu
@@ -1,4 +1,6 @@
+#include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 #include <stdio.h>
 #include <stdlib.h>
